698680d6a72fb7c9ea89ddb593897a158418037b
magnatestech <android.magnates@gmail.com>
2019-12-19 18:43:46 +0530

    Changed Structure & made with Three different Controls

--- chatenginesdklibrary/src/main/java/corechat/com/chatenginesdklibrary/MainClass.kt
+++ chatenginesdklibrary/src/main/java/corechat/com/chatenginesdklibrary/MainClass.kt
@@ -1,16 +1,52 @@
 package corechat.com.chatenginesdklibrary
 
 import android.content.Context
+import android.widget.Button
+import corechat.com.chatenginesdklibrary.Utils.SdkPreferencesManager
+import corechat.com.corechatdemo.custom_views.CC_CustomButton
+import android.graphics.Typeface
+import android.graphics.drawable.GradientDrawable
+import android.support.v4.content.ContextCompat
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
-class MainClass {
 
-    constructor(context: Context)
+class MainClass(context: Context) {
 
-//    companion object{
-        fun printMessage(msg : String, view: CC_CustomTextView){
-            view.init()
-            view.text = msg
-        }
-//    }
+    private val mContext : Context = context
 
+    val AppPrefs = SdkPreferencesManager(mContext)
+
+    fun setAppTheme(font: String, bgDrawable : Int){
+
+        CC_CustomButton(mContext)
+
+        AppPrefs.setAppFont(font)
+        AppPrefs.setBgDrawable(bgDrawable)
+    }
+
+    fun setCustomBg(view: View, bgDrawable : Int){
+        AppPrefs.setBgDrawable(bgDrawable)
+        view.setBackgroundResource(bgDrawable)
+    }
+
+    fun setCustomButtonFont(view: Button, font : String){
+        val font: Typeface = Typeface.createFromAsset(mContext.assets, font)
+        view.typeface = font
+    }
+
+    fun setCustomTextViewFont(view: TextView, font : String){
+        val font: Typeface = Typeface.createFromAsset(mContext.assets, font)
+        view.typeface = font
+    }
+
+    fun setCustomEditTextFont(view: EditText, font : String){
+        val font: Typeface = Typeface.createFromAsset(mContext.assets, font)
+        view.typeface = font
+    }
+
+    fun changeBgColor(view: View, color : Int){
+        (view.background as GradientDrawable).setColor(ContextCompat.getColor(mContext,color))
+    }
 }
